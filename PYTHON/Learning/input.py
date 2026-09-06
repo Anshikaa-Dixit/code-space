3f1561be0e5e4d6011e0c760d4c9a08a1ad42845
Anshika Dixit <anshikadixit09200@gmail.com>
2026-09-06 22:57:22 +0530

    DATTEBAYOO:^

--- PYTHON/Learning/input.py
+++ PYTHON/Learning/input.py
@@ -7,7 +7,8 @@ print(f"My name is {name}")
 print(f"I love watching animes and my favorite animes are {anime}")
 print(f"I have watched AOT {times} times.")
 
-age = int(input("How old are you?; "))
+age = int(input("How old are you?: "))
 print(f"I am {age} years old.")
 age += 1
-print(f"In 2027 i will be{age} years old:)")
+print(f"In 2027 i will be {age} years old:)")
+
